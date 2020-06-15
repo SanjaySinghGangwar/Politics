5c187db40ce432290bb131ba0234df003d4c9cb4
hgnis <gangwar.jar@gmail.com>
2020-06-15 16:12:12 +0530

    VERSION 1.0

---
+++ app/src/main/java/com/sanjaysgangwar/politics/SplashScreen.java
@@ -0,0 +1,14 @@
+package com.sanjaysgangwar.politics;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+    }
+}
