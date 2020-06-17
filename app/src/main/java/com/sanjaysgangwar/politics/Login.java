92a0f1fc3dd02294ce4a953954530d6f9357aa61
hgnis <gangwar.jar@gmail.com>
2020-06-17 15:07:14 +0530

    VERSION 1.1 Went To Home Screen.

--- app/src/main/java/com/sanjaysgangwar/politics/Login.java
+++ app/src/main/java/com/sanjaysgangwar/politics/Login.java
@@ -1,18 +1,57 @@
 package com.sanjaysgangwar.politics;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
 
 public class Login extends AppCompatActivity {
     EditText phoneNumber;
     Button verify;
+    String genCode;
+    FirebaseAuth auth;
+    PhoneAuthProvider.OnVerificationStateChangedCallbacks callback = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+        @Override
+        public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+            String code = phoneAuthCredential.getSmsCode();
+            if (code != null) {
+                verifyCode(code);
+            }
+        }
+
+        @Override
+        public void onVerificationFailed(@NonNull FirebaseException e) {
+            Toast.makeText(Login.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+            super.onCodeSent(s, forceResendingToken);
+            genCode = s;
+        }
+
+        @Override
+        public void onCodeAutoRetrievalTimeOut(@NonNull String s) {
+            super.onCodeAutoRetrievalTimeOut(s);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,16 +59,65 @@ public class Login extends AppCompatActivity {
         setContentView(R.layout.login);
         verify = findViewById(R.id.verifyBt);
         phoneNumber = findViewById(R.id.phoneNumber);
-
+        auth = FirebaseAuth.getInstance();
         verify.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(Login.this);
-                View view = getLayoutInflater().inflate(R.layout.terms_and_conditions, null);
-                builder.setView(view);
-                AlertDialog alertDialog = builder.create();
-                alertDialog.show();
+                String number = "+91" + phoneNumber.getText().toString().trim();
+                if (number.isEmpty() || number.length() > 13 || number.length() < 13) {
+                } else {
+                    MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(Login.this);
+                    View view = getLayoutInflater().inflate(R.layout.get_otp, null);
+                    builder.setView(view);
+                    AlertDialog alertDialog = builder.create();
+                    // alertDialog.setCancelable(false);
 
+                    //send the code then open the dialog
+                    VerifyPhone(number);
+                    alertDialog.show();
+                    final EditText otpNumber = view.findViewById(R.id.OTPet);
+                    Button otpBt = view.findViewById(R.id.OTPbt);
+
+
+                    otpBt.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            String otp = otpNumber.getText().toString().trim();
+                            if (otp.isEmpty()) {
+
+                            } else {
+                                verifyCode(otp);
+                            }
+                        }
+                    });
+
+
+                }
+
+
+            }
+        });
+    }
+
+    private void VerifyPhone(String number) {
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(number, 60, TimeUnit.SECONDS, Login.this, callback);
+    }
+
+    private void verifyCode(String otp) {
+        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(genCode, otp);
+        signInWithCredential(credential);
+    }
+
+    private void signInWithCredential(PhoneAuthCredential credential) {
+        auth.signInWithCredential(credential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                Intent intent = new Intent(Login.this, Home.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+                finish();
             }
         });
     }
--- app/src/main/java/com/sanjaysgangwar/politics/SplashScreen.java
+++ app/src/main/java/com/sanjaysgangwar/politics/SplashScreen.java
@@ -2,6 +2,7 @@ package com.sanjaysgangwar.politics;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,16 +19,29 @@ public class SplashScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen);
         user = FirebaseAuth.getInstance().getCurrentUser();
-        if (user == null) {
-            Log.i("MESSAGE", "No User Found");
-            Intent intent = new Intent(SplashScreen.this, Login.class);
-            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            startActivity(intent);
-            finish();
-        } else {
-            Log.i("MESSAGE", "User Found");
-        }
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (user == null) {
+                    Log.i("MESSAGE", "No User Found");
+                    Intent intent = new Intent(SplashScreen.this, Login.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    Log.i("MESSAGE", "User Found");
+                    Intent intent = new Intent(SplashScreen.this, Home.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        }, 500);
+
     }
 }
